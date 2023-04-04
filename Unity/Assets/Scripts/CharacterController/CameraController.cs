4565d36ba1cd8809dfe8bcd17468a5186f0c160c
MVDW <maxvdwolf@gmail.com>
2023-04-04 19:48:40 +0200

    latest unity stuff I forgor 💀 to push(gonna be unused anyway)

--- Unity/Assets/Scripts/CharacterController/CameraController.cs
+++ Unity/Assets/Scripts/CharacterController/CameraController.cs
@@ -10,11 +10,21 @@ public class CameraController : MonoBehaviour
     public Vector3 offset;
     public Vector3 currentVelocity = Vector3.zero;
 
-    
-    void LateUpdate()
-    {
-        Vector3 targetPosition = target.position + offset;
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref currentVelocity, smoothSpeed);
+    private bool started;
+
+    void Start() {
+        transform.position = target.position + offset;
+        started = true;
+    }
+
+    void LateUpdate() {
+        if(started){
+            Vector3 targetPosition = target.position + offset;
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref currentVelocity, smoothSpeed);
+            transform.LookAt(target);
+
+        }
+        
 
         //transform.LookAt(target);
     }
