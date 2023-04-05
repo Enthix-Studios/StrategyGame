d7d62e16977f5afa1ae40557f028f426dcc27c5c
MVDW <maxvdwolf@gmail.com>
2023-04-05 20:33:25 +0200

    Add basic server connection handling for MeID

--- Game/Scripts/Connection/ConnectionManager.gd
+++ Game/Scripts/Connection/ConnectionManager.gd
@@ -2,7 +2,14 @@ extends Node
 
 var websocket_url = "ws://enthix.net:30001"
 
+@export var SERVER_AUTH_TOKEN = "NONE"
+
+
 var socket = WebSocketPeer.new()
+var connection_state = 0
+
+
+
 
 func _ready():
 	print_debug("Connecting to ", websocket_url)
@@ -13,10 +20,14 @@ func _process(delta):
 	socket.poll()
 	var state = socket.get_ready_state()
 	if state == WebSocketPeer.STATE_OPEN:
-		socket.send_text("hi");
+		_connectionHandleSetup()
 		while socket.get_available_packet_count():
-			print("Packet: ", socket.get_packet())
-		
+			var event_packet_raw = socket.get_packet().get_string_from_utf8()
+			var event_packet_json = _processJsonFormat(event_packet_raw)
+			print_debug("[server] ", event_packet_raw)
+			_connectionHandleEvents(event_packet_json.event, event_packet_json.data)
+			
+			
 	elif state == WebSocketPeer.STATE_CLOSING:
 		pass
 	elif state == WebSocketPeer.STATE_CLOSED:
@@ -24,3 +35,24 @@ func _process(delta):
 		var reason = socket.get_close_reason()
 		print("WebSocket closed with code: %d, reason %s. Clean: %s" % [code, reason, code != -1])
 		set_process(false) # Stop processing.
+		
+func _processJsonFormat(json_string):
+	var json_object = JSON.new()
+	var json_data = json_object.parse(json_string)
+	return json_object.get_data()
+		
+
+	
+func _connectionHandleSetup():
+	if connection_state == 0:
+		socket.send_text('{ "event": "LOGIN_TOKEN_REQUEST", "data": null }');
+		connection_state = 1
+		
+func _connectionHandleEvents(event, data):
+	match(event):
+		"LOGIN_TOKEN_REFRESH":
+			SERVER_AUTH_TOKEN = data
+		"AUTH":
+			if data == "OK":
+				print("Client has been logged in.")
+	pass
--- Game/Scripts/Connection/MeID-button.gd
+++ Game/Scripts/Connection/MeID-button.gd
@@ -2,4 +2,15 @@ extends Button
 
 
 func _on_pressed():
-	OS.shell_open("https://www.enthix.net/strategy/login?token=NULL")
+	var globalScene = get_node("/root/Global")
+	var SERVER_AUTH_TOKEN = globalScene.SERVER_AUTH_TOKEN
+	OS.shell_open("https://www.enthix.net/strategy/login?token=" + SERVER_AUTH_TOKEN)
+
+
+func _on_mouse_entered():
+	mouse_default_cursor_shape = Control.CURSOR_POINTING_HAND
+
+
+func _on_mouse_exited():
+	mouse_default_cursor_shape = Control.CURSOR_ARROW
+
