3e0ee8991d4abaa96ac4162dddad4d0d740eddc7
MVDW <maxvdwolf@gmail.com>
2023-04-07 09:26:00 +0200

    Add play button

--- Game/Scripts/SceneManagers/MainMenuSceneManager.gd
+++ Game/Scripts/SceneManagers/MainMenuSceneManager.gd
@@ -9,3 +9,8 @@ func _process(delta):
 	elif SERVER_CONNECTION_STATE == 3:
 		get_node("/root/MainMenu/MeIDLoginButton").hide()
 		get_node("/root/MainMenu/PlayButton").show()
+
+
+func _on_play_button_pressed():
+	get_tree().change_scene("res://Scenes/World/1.tscn")
+	pass # Replace with function body.
